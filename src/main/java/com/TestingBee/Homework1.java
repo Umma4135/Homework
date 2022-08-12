1c67296025bdcb66d48c0fb74a7a22ee4b413ffc
umma4135 <umma4135@gmail.com>
2022-08-12 00:11:35 -0400

    push

---
+++ src/main/java/com/TestingBee/Homework1.java
@@ -0,0 +1,74 @@
+package com.TestingBee;
+
+public class Homework1 {
+	
+		public void task1() {
+			
+		}	
+		public void task2()	{
+		int x = 10;
+		System.out.println(x);
+		}
+		public static boolean task3()	{
+			int a = 10;
+			return false;
+			
+			
+		}
+		public void task4(int a) {
+			
+		}
+		public void task5(String u, String k) {
+			
+		}
+		public void task6(String u, int x) {
+			
+		}
+		public void  toask7(String u, int x, boolean y) {
+			
+		}
+		public int task8(int x, int c) {
+			return(x+c);
+			
+		}
+		public static boolean task9(String u, String k)	{
+			boolean b = u.equals(k);
+			return b;
+		}
+		public static void task10() {
+			int a=1;
+			while(a<=10) {
+			System.out.println(a);
+			a++;
+			}
+		}
+		
+		public static  String task11(String s, int a) {
+			String newone ="";
+			for(int i=0; i<a; i++) {
+				newone=newone+s;
+			}
+			return newone;
+				
+			}
+		public static void main(String[] args) {
+			Homework1.task3();
+			System.out.println(Homework1.task3());
+			Homework1.task9("Umma", "Kulsum");
+			System.out.println(Homework1.task9("Umma", "Kulsum"));
+			Homework1.task10();
+			Homework1.task11("Umma", 4);
+			System.out.println(Homework1.task11("Umma  ", 4));
+			
+			
+
+			}
+
+		}
+
+	
+		
+
+	
+
+
---
+++ src/main/java/com/TestingBee/TestingClass1.java
@@ -0,0 +1,15 @@
+package com.TestingBee;
+
+public class TestingClass1 {
+	
+public void get1() {
+//**	
+}	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
